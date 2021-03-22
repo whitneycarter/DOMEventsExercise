fe474df0b387ac03e12fa7fe988188c482e50d1b
Whitney Carter <whitneycarter@SCWDBs-MacBook-Pro-3.local>
2021-03-22 09:39:16 -0500

    completed exercise

---
+++ app.js
@@ -0,0 +1,18 @@
+// Selectors
+const button1 = document.querySelector(`#one`);
+
+const h3 = document.querySelector(`h3`);
+
+const form = document.querySelector(`form`);
+
+
+// Event Listeners
+button1.onclick = () => alert("Cool! You can hear/understand a hummingbird and ants");
+
+h3.addEventListener(`mouseover`, () => alert("OH NO! The squirrels are on to you. Now you have to leave this reality and move to a new one."));
+
+
+
+// Functions
+
+
